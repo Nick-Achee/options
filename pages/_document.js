5578092485f8728480606803d1610775c80b9c5f
Nick Achee <64217718+Nick-Achee@users.noreply.github.com>
2022-08-15 22:45:12 +0000

    mnew

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
-/*eslint eqeqeq:0*/
-
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+import Document, { Html, Head, Main} from 'next/document'
 
 class MyDocument extends Document {
   render() {
@@ -71,7 +70,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
 
-          <script
+          <Script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -79,7 +78,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NVF2VGC');`,
             }}
-          ></script>
+          ></Script>
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
           <Main />
