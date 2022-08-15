27d3f1bf999a1d0f27d9e030fc2a2c46ac94a982
Nick Achee <64217718+Nick-Achee@users.noreply.github.com>
2022-08-15 22:39:38 +0000

    new

--- pages/index.js
+++ pages/index.js
@@ -26,18 +26,18 @@ const Main = () => (
 <div className="content-center flex  sm:flex items-center justify-center flex-wrap space-x-4 h-screen ">
   
 <div className="p-2">
-  <a class="relative inline-block text-xl font-prag font-bold text-black group active:text-black focus:outline-none focus:ring" href="https://nickachee.xyz">
-  <span class="absolute inset-0 transition-transform translate-x-1 translate-y-1 bg-black group-hover:translate-y-0 group-hover:translate-x-0"></span>
+  <a className="relative inline-block text-xl font-prag font-bold text-black group active:text-black focus:outline-none focus:ring" href="https://nickachee.xyz">
+  <span className="absolute inset-0 transition-transform translate-x-1 translate-y-1 bg-black group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-  <span class="relative  block px-4 py-3 bg-white border-2 border-current">
+  <span className="relative  block px-4 py-3 bg-white border-2 border-current">
   Digital Strategy<span aria-hidden="true" className="ml-2" role="img">🎯</span>
     
   </span>
 </a></div><div className="p-2">
-<a class="relative inline-block text-xl font-prag font-bold text-black group active:text-black focus:outline-none focus:ring" href="https://susanchase.com/agents/nick-achee">
-  <span class="absolute inset-0 transition-transform translate-x-1 translate-y-1 bg-black group-hover:translate-y-0 group-hover:translate-x-0"></span>
+<a className="relative inline-block text-xl font-prag font-bold text-black group active:text-black focus:outline-none focus:ring" href="https://susanchase.com/agents/nick-achee">
+  <span className="absolute inset-0 transition-transform translate-x-1 translate-y-1 bg-black group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
-  <span class="relative block px-4 py-3 bg-white border-2 border-current">
+  <span className="relative block px-4 py-3 bg-white border-2 border-current">
   Real Estate<span aria-hidden="true" className="ml-2" role="img">🏡</span>
   </span>
 </a></div>
